f05524347c3fea34fc2a7b0bea9301973fc76276
Rwanda S <rwanda.ace@gmail.com>
2021-01-12 00:07:50 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,17 @@
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we", 
+    favorite_ice_cream_flavors: ["chocolate", "vanilla", "mint chip"],
+        knows: nil
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+ 
+contacts.each do |person, data|
+  puts "#{person}: #{data}"
+end
